a2d4ff848071c46296a06f9ee25ba1b9abb20616
mr-macintosh01 <65767905+mr-macintosh01@users.noreply.github.com>
2023-10-17 11:37:09 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,14 +1,14 @@
 let initialWindowHeight = window.innerHeight
 let initialWindowWidth = window.innerWidth
 
-let shipsNumber = 25
+let shipsNumber = 20
 
 if (window.innerWidth <= 1300 && window.innerWidth > 1000) {
-    shipsNumber = 15
+    shipsNumber = 10
 } else if (window.innerWidth <= 1000) {
     shipsNumber = 5
 } else {
-    shipsNumber = 25
+    shipsNumber = 20
 }
 
 const modes = ['linear', 'ease', 'ease-in', 'ease-out', 'ease-in-out']
